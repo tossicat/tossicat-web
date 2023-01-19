5f4f3ae35ceff45bbdc72e33c2c8879cd0ef8881
Jaehwan <dialektike@gmail.com>
2023-01-19 13:13:26 +0900

    테스트 코드 시작

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use tossicat::{modify_sentence, postfix};
+use wasm_bindgen::prelude::*;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -11,4 +14,34 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+}
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn fix(word: &str, tossi: &str) {
+    let temp = postfix(word, tossi);
+    let test = match temp {
+        Err(e) => Err(e),
+        Ok(temp) => Ok(temp),
+    };
+
+    alert(&format!(
+        "단어:{}, 토시:{}의 결과는 {:?}!",
+        word, tossi, test
+    ));
+}
+
+#[wasm_bindgen]
+pub fn fix_sentence(sentence: &str) {
+    let temp = modify_sentence(sentence);
+    let test = match temp {
+        Err(e) => Err(e),
+        Ok(temp) => Ok(temp),
+    };
+
+    alert(&format!("{} : {:?}!", sentence, test));
 }
